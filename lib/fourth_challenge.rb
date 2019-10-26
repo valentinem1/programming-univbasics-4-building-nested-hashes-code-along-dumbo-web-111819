e3c82a8a51986c267d07e2d86728e24b047d941b
Valentine Maillard <valentinemaillard1@gmail.com>
2019-10-26 22:05:51 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -12,8 +12,9 @@ def fourth_challenge
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
-      :heroine_friends => [
+      :heroine_friends => [:
         #your hashes here!
+        
       ]
    }
 }
