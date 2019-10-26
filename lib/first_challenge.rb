e3bf8a72fd32efdadfc6b9efac6062a76161a1eb
Valentine Maillard <valentinemaillard1@gmail.com>
2019-10-26 21:44:32 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,6 +1,6 @@
 def first_challenge
    epic_tragedy = {
-     :montague => {
+  :montague => {
       :patriarch => {
       :name => "Lord Montague",
       :age => "53"
@@ -13,6 +13,31 @@ def first_challenge
       :name => "Romeo",
       :age => "15"
     }
-     :capulet => {}
+    
+ :capulet => {
+       :patriarch => {
+      :name => "Lord Capulet",
+      :age => "50"
+    },
+    :matriarch => {
+      :name => "Lady Capulet",
+      :age => "51"
+    },
+    :heroine => {
+      :name => "Juliet",
+      :age => "15",
+      :status => "alive"
+    },
+    :heroine_friends=> [
+      {
+        :name => "Steven",
+        :age => "30",
+        :attitude => "confused"
+      },
+      {
+        :name => "Nurse",
+        :age => "44",
+        :attitude => "worried"
+     }
    }
 end
